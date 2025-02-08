2652ff8daf3bebb5dc6b36068b9eca459cc3c7ac
sppidy <sppidytg@gmail.com>
2025-02-08 15:25:16 +0530

    Electromart : redirect fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "X-Forwarded-For",
+            value: "$remote_addr",
+          },
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
